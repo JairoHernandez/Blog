f889c9e9e1b983b0a7e58610334fb889abdecbfd
JairoHernandez <jairomh@hotmail.com>
2017-11-02 23:06:22 -0500

    Action Creator Shortcuts

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions';
 
 class PostIndex extends Component {
+    
+    // Executes right after component is loaded in the DOM.
+    componentDidMount() {
+        this.props.fetchPosts();
+    }
+    
     render() {
         return (
             <div>
@@ -10,4 +18,7 @@ class PostIndex extends Component {
     }
 }
 
-export default PostIndex;
+
+// Shortcut to avoid use of mapDispatchToProps is by just passing in {fetchPosts}
+// If you require say a computation then you can then bring back use of mapDispatchToProps.
+export default connect(null, { fetchPosts })(PostIndex);
